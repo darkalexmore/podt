0ec6bb7283f8ab71c55f9215e43735f00e4672aa
darkalexmore <93031128+darkalexmore@users.noreply.github.com>
2022-10-11 15:25:24 +0530

    Update potd6o22.cpp

--- potd6o22.cpp
+++ potd6o22.cpp
@@ -1,10 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-// User function Template for C++
 class Solution {
   public:
     string lexicographicallySmallest(string S, int k) {
@@ -47,19 +40,3 @@ class Solution {
         return ss;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string S;
-        int k;
-        cin >> S >> k;
-        Solution ob;
-        cout << ob.lexicographicallySmallest(S, k) << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
