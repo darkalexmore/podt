473f98a9c9946374082e20ba0bb15efdeb6c048c
darkalexmore <93031128+darkalexmore@users.noreply.github.com>
2022-10-11 15:24:15 +0530

    Update potd11o22.cpp

--- potd11o22.cpp
+++ potd11o22.cpp
@@ -1,12 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-// User function Template for C++
-
 class Solution{
 public:
     string decodedString(string s){
@@ -47,19 +38,3 @@ public:
         return res;
     }
 };
-
-//{ Driver Code Starts.
-
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        string s;
-        cin>>s;
-        
-        Solution ob;
-        cout<<ob.decodedString(s)<<"\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
